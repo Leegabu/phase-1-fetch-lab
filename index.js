812d30d753a5169a6d293695862585aa998c87b7
Lee <leeirungu11313@gmail.com>
2024-04-04 09:27:53 +0300

    Add index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  let basedURL = "https://anapioficeandfire.com/api/books"
+  return fetch(basedURL)
+  .then((response) => (response.json()))
+  .then(books => renderBooks(books))
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
